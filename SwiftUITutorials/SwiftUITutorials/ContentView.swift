7bf7cf6e8636cb2aa2dc5abe8d2fc26be63fc672
Phonthep Aungkanukulwit <mm@Phontheps-MacBook-Air.local>
2021-09-06 13:05:29 +0700

    initial project swiftui

---
+++ SwiftUITutorials/SwiftUITutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorials
+//
+//  Created by Phonthep Aungkanukulwit on 6/9/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITutorials/SwiftUITutorials/SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialsApp.swift
+//  SwiftUITutorials
+//
+//  Created by Phonthep Aungkanukulwit on 6/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
