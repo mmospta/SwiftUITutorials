08feb20454148d78e0aded791de78a31f2327a0f
Phonthep Aungkanukulwit <mm@Phontheps-MacBook-Air.local>
2021-09-06 18:16:31 +0700

    creating and combining views

---
+++ SwiftUITutorials/SwiftUITutorials/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  SwiftUITutorials
+//
+//  Created by Phonthep Aungkanukulwit on 6/9/2564 BE.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
