1552222e86c99421de4e8599e5b4e2eede9d253d
Phonthep Aungkanukulwit <mm@Phontheps-MacBook-Air.local>
2021-09-07 00:25:00 +0700

    handling user input

--- SwiftUITutorials/SwiftUITutorials/SwiftUITutorialsApp.swift
+++ SwiftUITutorials/SwiftUITutorials/SwiftUITutorialsApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct SwiftUITutorialsApp: App {
+    @State private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
--- SwiftUITutorials/SwiftUITutorials/Views/Landmarks/LandmarkDetail.swift
+++ SwiftUITutorials/SwiftUITutorials/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     
     var body: some View {
         ScrollView {
@@ -23,8 +28,12 @@ struct LandmarkDetail: View {
                     .padding(.bottom, -130)
                 
                 VStack(alignment: .leading){
-                    Text(landmark.name)
-                        .font(.title)
+                    HStack {
+                        Text(landmark.name)
+                            .font(.title)
+                            .foregroundColor(.primary)
+                        FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                    }
                     
                     HStack {
                         Text(landmark.park)
@@ -51,7 +60,10 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
     }
 }
