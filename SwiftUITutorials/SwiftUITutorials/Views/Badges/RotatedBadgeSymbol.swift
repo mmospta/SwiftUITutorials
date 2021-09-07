d5ac5ea3e71237df98459f00d544871ef44cae6a
Phonthep Aungkanukulwit <mm@Phontheps-MacBook-Air.local>
2021-09-07 22:34:53 +0700

    animating views and transitions
    - GeometryReader
    - Path
    - CGPoint
    - UnitPoint
    - move to
    - addLine
    - addQuadCurve
    - effect rotate, scale
    - position
    - aspectRatio
    - gradient

---
+++ SwiftUITutorials/SwiftUITutorials/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotateBadgeSymbol.swift
+//  SwiftUITutorials
+//
+//  Created by Phonthep Aungkanukulwit on 7/9/2564 BE.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotateBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
