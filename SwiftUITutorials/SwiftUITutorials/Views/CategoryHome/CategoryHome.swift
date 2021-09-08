cd5a335e8f5e658659de2c7acea844301e8b6aae
Phonthep Aungkanukulwit <mm@Phontheps-MacBook-Air.local>
2021-09-09 01:23:40 +0700

    composing complex interfaces
    - ScrollView
    - TabView item tag selection
    - renderingMode
    - filter
    - Dictionary grouping by
    - enum CaseIterable

---
+++ SwiftUITutorials/SwiftUITutorials/Views/CategoryHome/CategoryHome.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryHome.swift
+//  SwiftUITutorials
+//
+//  Created by Phonthep Aungkanukulwit on 8/9/2564 BE.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+            }
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
---
+++ SwiftUITutorials/SwiftUITutorials/Views/CategoryHome/CategoryItem.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryItem.swift
+//  SwiftUITutorials
+//
+//  Created by Phonthep Aungkanukulwit on 8/9/2564 BE.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 115, height: 115)
+                .cornerRadius(5)
+            
+            Text(landmark.name)
+                .foregroundColor(.primary)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
