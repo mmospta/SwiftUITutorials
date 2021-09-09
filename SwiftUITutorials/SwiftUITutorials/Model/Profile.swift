ebf75dc169511b338e6cb8f32c65219354b47a4f
Phonthep Aungkanukulwit <mm@Phontheps-MacBook-Air.local>
2021-09-10 02:31:47 +0700

    working with ui controls
    - listStyle
    - listRowInset
    - EdgeInsets()
    - toolbar
    - sheet
    - binding
    - Picker SegmentPickerStyle
    - DatePicker
    - dateRange
    - @Environment \.editMode
    - EditButton
    - onAppear onDisAppear

---
+++ SwiftUITutorials/SwiftUITutorials/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  SwiftUITutorials
+//
+//  Created by Phonthep Aungkanukulwit on 9/9/2564 BE.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumer")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { self.rawValue }
+    }
+}
--- SwiftUITutorials/SwiftUITutorials/Views/CategoryHome/CategoryHome.swift
+++ SwiftUITutorials/SwiftUITutorials/Views/CategoryHome/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationView {
@@ -18,12 +19,25 @@ struct CategoryHome: View {
                     .scaledToFill()
                     .frame(height: 200)
                     .clipped()
+                    .listRowInsets(EdgeInsets())
                 
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
             }
-                .navigationTitle("Featured")
+            .listStyle(InsetListStyle())
+            .navigationTitle("Featured")
+            .toolbar {
+                Button(action: { showingProfile.toggle() }) {
+                    Image(systemName: "person.crop.circle")
+                        .accessibilityLabel("User Profile")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
     }
 }
